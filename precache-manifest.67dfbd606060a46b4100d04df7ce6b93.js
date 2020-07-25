388e0d318911e79dca97a9668cddf79c5800d453
SHOBHIT MAHESHWARI <shobhitrocks1103@gmail.com>
2020-07-25 20:40:11 +0530

    Updates

---
+++ precache-manifest.67dfbd606060a46b4100d04df7ce6b93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d5215631c229fb6b48496b420dc953",
+    "url": "/My-React-Todo-App/index.html"
+  },
+  {
+    "revision": "096bb8dd7e9ef00758ec",
+    "url": "/My-React-Todo-App/static/css/main.6fcdac9a.chunk.css"
+  },
+  {
+    "revision": "1e2ea23db34195546dc4",
+    "url": "/My-React-Todo-App/static/js/2.5d465b26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-React-Todo-App/static/js/2.5d465b26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096bb8dd7e9ef00758ec",
+    "url": "/My-React-Todo-App/static/js/main.dfa698c1.chunk.js"
+  },
+  {
+    "revision": "94bb39343957ceb8f4d1",
+    "url": "/My-React-Todo-App/static/js/runtime-main.b8873e8b.js"
+  },
+  {
+    "revision": "eac5b6ad7afd89cb4c255089ce81ef22",
+    "url": "/My-React-Todo-App/static/media/logo-main.eac5b6ad.jpg"
+  }
+]);
